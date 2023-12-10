79953724caef168ce384a2eb4e367436fb960559
Zufiya Idrisi <idrisizufiya@gmail.com>
2023-12-10 19:26:00 +0530

    Routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,3 @@
-// import React from 'react';
-// import './App.css';
-// import Sidebar from './Components/Sidebar';
-// import Main from './Components/Main';
-
-// const App = () => {
-//   return (
-//     <div className="app-container">
-//       <div className="grid-container">
-//         <Sidebar />
-//         <Main />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default App;
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Companies from './Pages/Companies';
@@ -22,20 +5,25 @@ import Home from './Pages/Home';
 import Settings from './Pages/Settings';
 import Logout from './Pages/Logout';
 import Main from './Components/Main';
-import Sidebar from './Components/Sidebar'
+import Sidebar from './Components/Sidebar';
+import "./App.css"
 
 const App = () => {
   return (
+    <Router> {/* Use <Router> instead of <BrowserRouter> */}
       <div className="app-container">
-        <Sidebar />
-        <Routes>
-          <Route path="/dashboard" element={<Main />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/companies" element={<Companies />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/logout" element={<Logout />} />
-        </Routes>
+        <div className="grid-container">
+          <Sidebar />
+          <Routes>
+            <Route path="/dashboard" element={<Main />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/companies" element={<Companies />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/logout" element={<Logout />} />
+          </Routes>
+        </div>
       </div>
+    </Router>
   );
 }
 
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,59 +1,27 @@
-// import React from 'react'
-// import { FaHome } from "react-icons/fa";
-// import { IoBagCheckSharp } from "react-icons/io5";
-// import { MdOutlinePeopleAlt } from "react-icons/md";
-// import { FaPowerOff } from "react-icons/fa";
-// import { IoMdSettings } from "react-icons/io";
-
-// const Sidebar = () => {
-//   return (
-//     <div className="grid-container">
-//       <div className="sidebar">
-//         <div className="top-icons">
-//           <FaHome className="icon" />
-//           <IoBagCheckSharp className="icon" />
-//           <MdOutlinePeopleAlt className="icon" />
-//           <IoMdSettings className="icon" />
-//         </div>
-//         <div className="bottom-icon">
-//         <FaPowerOff className="icon" />
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Sidebar;
-
-import React from 'react'
+import React from 'react';
 import { FaHome } from "react-icons/fa";
 import { MdOutlineDashboard } from "react-icons/md";
 import { MdOutlinePeopleAlt } from "react-icons/md";
 import { FaPowerOff } from "react-icons/fa";
 import { IoMdSettings } from "react-icons/io";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <>
-      <BrowserRouter>
-        <div className="grid-container">
-          <div className="sidebar">
-            <div className="top-icons">
-              <Link to="/"><FaHome className="icon" /></Link>
-              <Link to="/dashboard"><MdOutlineDashboard className="icon" /></Link>
-              <Link to="/companies"><MdOutlinePeopleAlt className="icon" /></Link>
-              <Link to="/settings"><IoMdSettings className="icon" /></Link>
-            </div>
-            <div className="bottom-icon">
-              <Link to="/logout"><FaPowerOff className="icon" /></Link>
-            </div>
-          </div>
+    <div className="grid-container">
+      <div className="sidebar">
+        <div className="top-icons">
+          <Link to="/"><FaHome className="icon" /></Link>
+          <Link to="/dashboard"><MdOutlineDashboard className="icon" /></Link>
+          <Link to="/companies"><MdOutlinePeopleAlt className="icon" /></Link>
+          <Link to="/settings"><IoMdSettings className="icon" /></Link>
         </div>
-      </BrowserRouter>
-    </>
-  )
+        <div className="bottom-icon">
+          <Link to="/logout"><FaPowerOff className="icon" /></Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
-
