1f9868656c5faa3eddf0d3469718183f8aba7fb4
Zufiya Idrisi <idrisizufiya@gmail.com>
2023-12-10 18:40:57 +0530

    Started adding routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,42 @@
-import React from 'react';
-import './App.css';
-import Sidebar from './Components/Sidebar';
-import Main from './Components/Main';
+// import React from 'react';
+// import './App.css';
+// import Sidebar from './Components/Sidebar';
+// import Main from './Components/Main';
 
+// const App = () => {
+//   return (
+//     <div className="app-container">
+//       <div className="grid-container">
+//         <Sidebar />
+//         <Main />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Companies from './Pages/Companies';
+import Home from './Pages/Home';
+import Settings from './Pages/Settings';
+import Logout from './Pages/Logout';
+import Main from './Components/Main';
+import Sidebar from './Components/Sidebar'
 
 const App = () => {
   return (
-    <div className="app-container">
-      <div className="grid-container">
+      <div className="app-container">
         <Sidebar />
-        <Main />
+        <Routes>
+          <Route path="/dashboard" element={<Main />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/companies" element={<Companies />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/logout" element={<Logout />} />
+        </Routes>
       </div>
-    </div>
   );
-};
+}
 
 export default App;
---
+++ src/Pages/Settings.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Settings = () => {
+  return (
+    <div>
+      <h1>Logout</h1>
+    </div>
+  )
+}
+
+export default Settings
